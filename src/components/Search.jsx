690c3ac73f4d5ee568adcaac4886624af8b5e749
Dimas Ahmad Rizal <fahrudinasevenfold@gmail.com>
2024-12-13 15:20:02 +0700

    add onclick method and keydown enter

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,15 +2,32 @@ import { useState } from "react";
 
 function search(props) {
   const [search, setSearch] = useState("");
-  const changeSearch = (event) => {
-    setSearch(event.target.value);
-    props.onChange(event.target.value);
+
+  const changeSearch = () => {
+    props.onChange(search);
   };
+
+  const searchKeydown = (event) => {
+    if (event.key === "Enter") {
+      changeSearch();
+    }
+  };
+
+  //   const changeSearch = (event) => {
+  //     setSearch(event.target.value);
+  //     props.onChange(event.target.value);
+  //     };
+  // component function ini digantikan dengan onChange={changeSearch} pada button
 
   return (
     <>
       <div>
-        Cari artikel : <input onChange={changeSearch} />
+        Cari artikel :{" "}
+        <input
+          onChange={(event) => setSearch(event.target.value)}
+          onKeyDown={searchKeydown}
+        />{" "}
+        <button onClick={changeSearch}>Cari</button>
       </div>
       <small>
         Ditemukan {props.totalPosts} data dengan pencarian : {search}
