721a24965682f0c4082d2dcbde6b8e5d2bd3087a
Dimas Ahmad Rizal <fahrudinasevenfold@gmail.com>
2024-12-13 14:08:41 +0700

    useState react

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,25 @@
+import { useState } from "react";
 import Article from "../components/Article";
 import posts from "../posts.json";
 
 function Homepage() {
+  const [search, setSearch] = useState("");
+
+  const changeSearch = (event) => {
+    setSearch(event.target.value);
+  };
+
   return (
-    <>
-      <h1>Welcome to the homepage</h1>
-      {posts.map((blog) => (
-        <Article title={blog.title} tags={blog.tags} date={blog.date} />
+    <div>
+      <h1>Welcome to the Blogs</h1>
+      <div>
+        Cari artikel : <input onChange={changeSearch} />
+      </div>
+      <small>Ditemukan data dengan pencarian {search}</small>
+      {posts.map(({ title, tags, date }) => (
+        <Article {...{ title, tags, date }} key={title} />
       ))}
-    </>
+    </div>
   );
 }
 
